b72e55649875d8e859b8bbaacc9c0515264f6a80
Derek <51767748+DerekLeeCS@users.noreply.github.com>
2021-10-29 22:04:14 -0400

    Create SVGWriter to wrap drawing functions

--- util/mesh_to_svg.py
+++ util/mesh_to_svg.py
@@ -2,37 +2,46 @@ from typing import List, Tuple
 import cairo
 
 Pair = Tuple[int, int]
-IMG_SIZE = 500
 
 
-def draw_triangles(file_name: str, triangles: List[Tuple[Pair, Pair, Pair]]) -> None:
-    with cairo.SVGSurface(file_name, IMG_SIZE, IMG_SIZE) as surface:
-        ctx = cairo.Context(surface)
-        ctx.set_line_width(1)
-        for triangle in triangles:
-            _draw_triangle(ctx, triangle)
+class SVGWriter():
+    def __init__(self, img_size: int, line_width: int) -> None:
+        self.img_size = img_size
+        self.line_width = line_width
 
 
-def _draw_triangle(ctx: cairo.Context, triangle_points: Tuple[Pair, Pair, Pair]) -> None:
-    # Extract points
-    point1, point2, point3 = triangle_points
-    x1, y1 = point1
-    x2, y2 = point2
-    x3, y3 = point3
+    def draw_triangles(self, file_name: str, triangles: List[Tuple[Pair, Pair, Pair]]) -> None:
+        with cairo.SVGSurface(file_name, self.img_size, self.img_size) as surface:
+            ctx = cairo.Context(surface)
+            ctx.set_line_width(self.line_width)
+            for triangle in triangles:
+                self._draw_triangle(ctx, triangle)
 
-    # Draw triangle
-    ctx.new_path()
-    ctx.move_to(x1, y1)
-    ctx.line_to(x2, y2)
-    ctx.line_to(x3, y3)
-    ctx.close_path()
-    ctx.stroke()
+
+    def _draw_triangle(self, ctx: cairo.Context, triangle_points: Tuple[Pair, Pair, Pair]) -> None:
+        # Extract points
+        point1, point2, point3 = triangle_points
+        x1, y1 = point1
+        x2, y2 = point2
+        x3, y3 = point3
+
+        # Draw triangle
+        ctx.new_path()
+        ctx.move_to(x1, y1)
+        ctx.line_to(x2, y2)
+        ctx.line_to(x3, y3)
+        ctx.close_path()
+        ctx.stroke()
 
 
 if __name__ == '__main__':
+    img_size = 500
+    line_width = 1
+    converter = SVGWriter(img_size, line_width)
+
     file_name = 'example.svg'
     triangles = [
         ((240, 40), (240, 160), (350, 160)),
         ((100, 20), (100, 50), (50, 50)),
     ]
-    draw_triangles(file_name, triangles)
+    converter.draw_triangles(file_name, triangles)
