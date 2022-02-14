d911a17c5195126bcddf1ed1db537f4e7d8e5658
Victoooooor <victorzh716@gmail.com>
2022-02-14 12:10:45 -0500

    rebase

---
+++ CMakeFiles/BN_Sample.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "BN_Sample.cpython-310-x86_64-linux-gnu.so"
+  "BN_Sample.pdb"
+  "CMakeFiles/BN_Sample.dir/src/sampler/imagesampler.cpp.o"
+  "CMakeFiles/BN_Sample.dir/src/sampler/imagesampler.cpp.o.d"
+  "CMakeFiles/BN_Sample.dir/src/sampler/pyboost_cv2_converter.cpp.o"
+  "CMakeFiles/BN_Sample.dir/src/sampler/pyboost_cv2_converter.cpp.o.d"
+  "CMakeFiles/BN_Sample.dir/src/sampler/pyboost_cv3_converter.cpp.o"
+  "CMakeFiles/BN_Sample.dir/src/sampler/pyboost_cv3_converter.cpp.o.d"
+  "CMakeFiles/BN_Sample.dir/src/sampler/pyboost_cv4_converter.cpp.o"
+  "CMakeFiles/BN_Sample.dir/src/sampler/pyboost_cv4_converter.cpp.o.d"
+  "CMakeFiles/BN_Sample.dir/src/sampler/python_module.cpp.o"
+  "CMakeFiles/BN_Sample.dir/src/sampler/python_module.cpp.o.d"
+  "CMakeFiles/BN_Sample.dir/src/sampler/quasisampler_prototype.cpp.o"
+  "CMakeFiles/BN_Sample.dir/src/sampler/quasisampler_prototype.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BN_Sample.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/BN_Sample.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for BN_Sample.
+# This may be replaced when dependencies are built.
